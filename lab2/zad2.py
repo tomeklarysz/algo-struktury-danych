7bd4c0ec4867a83ab72fd54ee5b8c131960530c5
tomek.larysz <ttomek.larysz@gmail.com>
2024-03-23 15:51:13 +0100

    zad2 finished

--- lab2/zad2.py
+++ lab2/zad2.py
@@ -1,27 +1,40 @@
 f = open('zadanie2.csv', 'r')
 
 myList = []
-txt = f.read()
-lines = txt.splitlines()
 
-
-print(f'starting length: {len(lines)}')
-for line in lines:
+count = 0
+for line in f:
+    if count == 0:
+        count += 1
+        continue
+    line = line.replace('\n', '')
     new_line = line.split(',')
+    id = int(new_line[0])
     val = new_line[1]
-    # print(new_line)
     if val != '':
-        myList.append(new_line)
-
-        
-# print(myList[-2])
-print(f'new length: {len(myList)}')
-
-for i in range(1, len(myList)):
-    myList[i][0] = int(myList[i][0])
-
+        myList.append([id, val])
+    count += 1
 
 myList.sort()
-print(myList)
+
+deletedWords = []
+for i in range(len(myList)):
+    myList[i][1] = myList[i][1].lower()
+    if i+1 < len(myList) and myList[i][0] >= myList[i+1][0]:
+        myList[i+1][0] = myList[i][0] + 1
+    words = myList[i][1].split(' ')
+    for word in words:
+        if len(word) >= 2:
+            prefix1 = word[0]
+            prefix2 = word[1]
+            if abs(ord(prefix1) - ord(prefix2)) == 1:
+                deletedWords.append([myList[i][0], word])
+                words.remove(word)
+    myList[i][1] = ''
+    myList[i][1] = ' '.join(words)
+    # print(myList[i])
+
+for d in deletedWords:
+    print(d)
 
 f.close()
