f4e01c60beb5bebd8f477a40461c8d577d3a6074
tomek.larysz <ttomek.larysz@gmail.com>
2024-05-07 11:24:05 +0200

    second approach on displaying

--- lab6/tree.py
+++ lab6/tree.py
@@ -25,21 +25,46 @@ nodes[3].right.left = Node(root, 7.7)
 nodes[3].right.right = 7.9
 nodes[3].right.left.left = 7.6
 
+nodes[4].left = 9.3
+
+# def printNodes(node, level):
+    # if isinstance(node, float):
+        # print(node)
+    # else:
+        # print(node.value, end='')
+        # if node.left:
+            # print("-"*level, end='')
+            # if not node.right:
+                # printNodes(node.left, level+1)
+            # else:
+                # print(node.left)
+                # print("   "*level, end='')
+                # if isinstance(node.right, float):
+                    # print(f'{"-"*level}{node.right}')
+                # else:
+                    # print("-"*level, end='')
+                    # printNodes(node.right, level+1)
+        # elif node.right:
+            # print("-"*level, end='')
+            # printNodes(node.right, level+1)
+        # else:
+            # print(node.value)
 
 def printNodes(node, level):
-    print(node.value, end='')
-    if node.left:
-        print("-"*level, end='')
-        if not node.right:
-            printNodes(node.left, level+1)
-        else:
-            print(node.left)
-            print(f'{" "*level}{"-"*level}{node.right}')
-    elif node.right:
-        print("-"*level, end='')
+    if node is None:
+        # print()
+        return
+    elif not isinstance(node, float):
+        print(node.value, end='')
+        print('-'*level, end='')
+        printNodes(node.left, level+1)
+        print('   '*level, end='')
+        print('-'*level, end='')
         printNodes(node.right, level+1)
     else:
-        print(node.value)
+        print(node)
+
+
 
 def printTree(tree):
     level = 1
