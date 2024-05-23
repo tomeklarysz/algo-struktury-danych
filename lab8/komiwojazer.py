b795c4a55daa6ccb84d908b9b0af130939456cf9
tomek.larysz <ttomek.larysz@gmail.com>
2024-05-23 10:42:49 +0200

    greedy finished

--- lab8/komiwojazer.py
+++ lab8/komiwojazer.py
@@ -9,10 +9,8 @@ for l in f:
     x.pop(0)
     cords.append(x)
 
-# print(cords)
-
-path = np.array(range(100))
-random.shuffle(path)
+# path = np.array(range(100))
+# random.shuffle(path)
 
 
 def path_length(path):
@@ -28,38 +26,43 @@ def path_length(path):
         length += math.sqrt(cur)
     return length
 
-# print(path_length([1, 2]))
-# print(path_length(path))
+
+# z = path_length(path)
+# print(f'random path length: {z}')
 
 # w kolejnosci takiej jak w pliku
 x = path_length(range(100))
-print(x)
+print(f'dlugosc po kolei: {x}')
 
 # greedy
 def greedy(cords):
     
-    start = cords.pop(0)
-    end = 0
+    visited = []
+    start = cords[0]
     length = 0
     current_index = 0
-    while len(cords) > 0:
+    while len(visited) < len(cords) - 1:
         min = 0
         index = 0
         for i in range(len(cords)):
-            cur = pow(float(cords[current_index][0]) - float(cords[i][0]), 2)
-            cur += pow(float(cords[current_index][1]) - float(cords[i][1]), 2)
-            cur = math.sqrt(cur)
-            if min == 0:
-                min = cur
-                index = i
-            else:
-                if cur < min:
+            if (i not in visited) and i != current_index:
+                cur = pow(float(cords[current_index][0]) - float(cords[i][0]), 2)
+                cur += pow(float(cords[current_index][1]) - float(cords[i][1]), 2)
+                cur = math.sqrt(cur)
+                if min == 0:
                     min = cur
                     index = i
-        if len(cords) == 0:
-            end = cords[current_index]
-        cords.pop(current_index)
+                else:
+                    if cur < min:
+                        min = cur
+                        index = i
+        visited.append(current_index)
         current_index = index
         length += min
-    # adding start
-            
+    cur = pow(float(cords[current_index][0]) - float(start[0]), 2)
+    cur += pow(float(cords[current_index][1]) - float(start[1]), 2)
+    length += math.sqrt(cur)
+    return length
+
+y = greedy(cords)
+print(f'metoda zachlanna: {y}')
